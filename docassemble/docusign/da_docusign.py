451331fdf464942a83fa3ecc88c6fc7c4cbd3d5b
wiersholm-sike <sike@wiersholm.no>
2024-03-30 23:29:15 +0100

    edit access token json load

--- docassemble/docusign/da_docusign.py
+++ docassemble/docusign/da_docusign.py
@@ -128,7 +128,7 @@ class DocuSign:
             'assertion': self.jwt_code
         })
         log(request_for_token.text)
-        self.token = request_for_token.json().get('access_token')
+        self.token = json.loads(request_for_token.text)['access_token']
     
     def get_user_info(self):
         if self.test_mode:
